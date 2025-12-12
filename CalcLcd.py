114c3c73f0b1194ebd41824af8b972b5fe1c5704
Alexander <pchp250g8@tutamail.com>
2025-12-12 13:59:22 +0300

    CalcLcm

---
+++ CalcLcd.py
@@ -0,0 +1,24 @@
+import os
+
+os.system("cls")
+print("Input the first integer number:")
+iNum1 = int(input())
+print("Input the second integer number:")
+iNum2 = int(input())
+iVal1 = abs(iNum1)
+iVal2 = abs(iNum2)
+if (iVal1 > 0 and iVal2 > 0):
+    iVal3 = iVal1
+    iVal4 = iVal2
+    while (iVal1 != iVal2):
+        if(iVal1 > iVal2):
+            iVal1 -= iVal2
+            iVal3 += iVal4
+        else:
+            iVal2 -= iVal1
+            iVal4 += iVal3
+    
+    nLcm = (iVal3 + iVal4) / 2
+    print(f"The Lcm of the numbers {iNum1} and {iNum2} is {nLcm}\r\n")
+else:
+    print("The numbers must not be equal 0")
